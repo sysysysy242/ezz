7c754eec25fd97af5d36066e943577328752365f
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2020-02-28 14:17:06 +0100

    Add files via upload

---
+++ .gitignore/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    token:          process.env.TOKEN,
+    shardCount:     0, //touche pâs
+
+    emojis: {
+        success:    "✅", //touche pâs
+        error:      "❎", //touche pâs
+        online:     "XXXXXXXXXXX", //touche pâs
+        dnd:        "XXXXXXXXXXX", //touche pâs
+        offline:    "XXXXXXXXXXX", //touche pâs
+        idle:       "XXXXXXXXXXX", //touche pâs
+        loading:    "⏲️", //touche pâs
+        warn:       "⚠️" //touche pâs
+    },
+
+    color:          "#eb2b1f", //touche pâs
+    footer:         "BY SYSY", //touche pâs
+
+    owners: [ "630341759997706241", "669636514728378378" ],
+
+    discord:        "XXXXXXXXXXX",
+    prefix:         "&",
+};
